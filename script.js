0c3a80069a3272471179d2a179dbae9652796b5c
tiagombp <tiago.pereira@tesouro.gov.br>
2026-04-15 14:48:17 -0300

    implementa a criação dos quadradinhos

--- script.js
+++ script.js
@@ -9,19 +9,22 @@ const data = fetch("data.json").then(output => output.json())
 
 class Square {
 
+    // o que preciso em cada quadradinho?
+    // as suas classificações, e os seus índices dentro das classificações.
+
     pos; // { x, y };
     
-    cls; // { funcao, gnd, rp };
+    classif; // { funcao, gnd, rp };
 
     index; // { geral, funcao, gnd, rp };
 
     constructor( 
-        { funcao, gnd, rp }
+        classificacoes, indices
     ) {
 
         //this.pos = { x, y };
-        this.cls = { funcao, gnd, rp };
-        //this.index = { geral, funcao, gnd, rp };
+        this.classif = classificacoes;
+        this.index = indices;
 
     }
 
@@ -31,7 +34,9 @@ let squares = [];
 
 function prepara_estruturas(data) {
 
-    let n_geral = 0, n_funcao = 0, n_gnd = 0, n_rp = 0;
+    // prepara estrutura para ir registrando os contadores dos indices de cada quadradinho dentro de cada classificacao
+
+    let n = 0; // indice geral
 
     const parametros = {
         dominios : {},
@@ -42,8 +47,10 @@ function prepara_estruturas(data) {
 
     classificacoes.forEach(classificacao => {
 
+        // pega os dominios de cada variável / classificação
         parametros.dominios[classificacao] = data.map(d => d[classificacao]).filter( (d,i,a) => a.indexOf(d) == i );
 
+        // a partir dos domínios, inicializa um índice para cada classificação
         parametros.indices[classificacao] = {};
 
         parametros.dominios[classificacao].forEach(categoria => {
@@ -56,8 +63,7 @@ function prepara_estruturas(data) {
 
     console.log(parametros);
 
-    /*
-
+    // processa os dados e vai criando os quadradinhos
     data.forEach( linha => {
 
         const funcao = linha["Função Governo Nome"];
@@ -66,17 +72,46 @@ function prepara_estruturas(data) {
 
         const qde_quadradinhos = linha.squares;
 
+        // cria estrutura que vai armazenar as classificações dos quadradinhos
+        // para uma mesma linha de dados, todos os quadradinhos terao classificadores iguais, então vamos criar essa estrutura aqui, para ser válida para toda a linha
+        const classifs = {};
+
+        // ou seja, pega o valor de cada variável de classificação para essa linha, e armazena num objeto
+        classificacoes.forEach(classif => classifs[classif] = linha[classif]);
+
+        //console.log(classifs);
+
         for (let i = 0; i < qde_quadradinhos; i++) {
 
-            const sq = new Square(
-                { funcao, gnd, rp },
-                { n_geral, n_funcao, n_gnd, n_rp }
+            const indices_deste_quadradinho = {};
 
+            indices_deste_quadradinho["geral"] = n;
+            n++;
+
+            classificacoes.forEach(classif => {
+
+                const valor_do_classificador_atual = classifs[classif];
+                
+                // pega o índice atual para esse classificador, para este valor específico
+                indices_deste_quadradinho[classif] = parametros.indices[classif][valor_do_classificador_atual];
+
+                // incrementa o índice no controle geral
+                parametros.indices[classif][valor_do_classificador_atual]++;
+
+            });
+
+            // cria um quadradinho para cada
+
+            const sq = new Square(
+                classifs,
+                indices_deste_quadradinho
             )
+
+            squares.push(sq);
 
         }
 
-    }) */
+    }) 
 
 
 
